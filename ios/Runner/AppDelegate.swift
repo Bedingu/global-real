94ffc2db7263fa7cf97b333c4c01f81a87a6e5e1
Gustavo Bedin <bedingelo@gmail.com>
2026-05-09 21:26:06 -0300

    fix: remove Firebase nativo do AppDelegate - deixa plugin Flutter gerenciar (v+31)

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,6 @@
 import Flutter
 import UIKit
 import GoogleMaps
-import FirebaseCore
-import FirebaseMessaging
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,18 +8,11 @@ import FirebaseMessaging
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 1) Registrar plugins primeiro
-    GeneratedPluginRegistrant.register(with: self)
-    
-    // 2) Firebase
-    FirebaseApp.configure()
-    
-    // 3) Google Maps
+    // Google Maps
     GMSServices.provideAPIKey("AIzaSyAfkTMK6054qNC78q6p-UBv3BF8ig9EmVQ")
     
-    // 4) Push notifications
-    UNUserNotificationCenter.current().delegate = self
-    application.registerForRemoteNotifications()
+    // Flutter plugins
+    GeneratedPluginRegistrant.register(with: self)
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
